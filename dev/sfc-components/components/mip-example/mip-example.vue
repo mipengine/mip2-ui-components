f5efa4894080bdc61d30b67d0f58987b72ad5651
chenqiushi <chenqiushi@baidu.com>
2018-08-06 16:46:46 +0800

    add demo of single file vue component

---
+++ dev/sfc-components/components/mip-example/mip-example.vue
@@ -0,0 +1,20 @@
+<template>
+  <mip-v-app>
+    <mip-v-btn color="info">button in .vue</mip-v-btn>
+  </mip-v-app>
+</template>
+
+<style scoped>
+.wrapper {
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
+
+<script>
+export default {
+  mounted () {
+    console.log('This is my first custom component !')
+  }
+}
+</script>
