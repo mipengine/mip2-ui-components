d82acb5d4c4ac0d0f6d8507bbf69de506923a27b
chenqiushi <tracy909@126.com>
2018-08-07 17:38:52 +0800

    run unit test

--- dev/sfc-components/components/mip-example/mip-example.vue
+++ dev/sfc-components/components/mip-example/mip-example.vue
@@ -1,5 +1,22 @@
 <template>
-  <mip-v-btn color="info">button in .vue</mip-v-btn>
+  <div>
+    <mip-v-btn color="info" class="green--text">button in .vue</mip-v-btn>
+
+    <mip-v-chip close="true">
+      <mip-v-avatar class="green">A</mip-v-avatar>ANZ Bank
+    </mip-v-chip>
+
+    <mip-v-chip outline="true" color="primary" tile="false" size="40">A</mip-v-chip>
+
+    <mip-v-avatar tile="false" size="40" color="red lighten-4">
+      <mip-img src="https://vuetifyjs.com/apple-touch-icon-180x180.png" alt="avatar"/>
+    </mip-v-avatar>
+
+    <mip-v-chip close="true">
+      <mip-v-avatar><img src="https://randomuser.me/api/portraits/men/35.jpg" alt="trevor">A</mip-v-avatar>
+    </mip-v-chip>
+
+  </div>
 </template>
 
 <style scoped>
