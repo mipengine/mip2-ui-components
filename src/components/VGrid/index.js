add4cc4ac3ee9dfb91a8ec455d999ebaf729f0b5
yenshih <yenshih95@gmail.com>
2018-08-14 17:06:17 +0800

    Update v-card

--- src/components/VGrid/index.js
+++ src/components/VGrid/index.js
@@ -3,15 +3,22 @@ import VContent from './VContent'
 import VContainer from './VContainer'
 import VFlex from './VFlex'
 import VLayout from './VLayout'
-const VSpacer = createSimpleFunctional('spacer', 'div', 'v-spacer')
-export { VContainer, VContent, VFlex, VLayout, VSpacer }
-const VGrid = {}
-/* istanbul ignore next */
-VGrid.install = function install (Vue) {
+const VSpacer = createSimpleFunctional('v-spacer')
+
+VContainer.install = function install (Vue) {
+  Vue.component(Vcontainer.name, VContainer)
+}
+VContent.install = function install (Vue) {
   Vue.component(VContent.name, VContent)
-  Vue.component(VContainer.name, VContainer)
+}
+VFlex.install = function install (Vue) {
   Vue.component(VFlex.name, VFlex)
+}
+VLayout.install = function install (Vue) {
   Vue.component(VLayout.name, VLayout)
+}
+VSpacer.install = function install (Vue) {
   Vue.component(VSpacer.name, VSpacer)
 }
-export default VGrid
+
+export { VContainer, VContent, VFlex, VLayout, VSpacer }
