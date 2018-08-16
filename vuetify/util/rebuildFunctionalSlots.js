a1aadc4dd462a452674e340c23de80f8b7b839d6
qiusiqi <qiusiqi@baidu.com>
2018-08-16 15:38:42 +0800

    text-field + css + code style

--- vuetify/util/rebuildFunctionalSlots.js
+++ vuetify/util/rebuildFunctionalSlots.js
@@ -4,12 +4,12 @@
  * @param {function} h
  * @returns {array}
  */
-export default function rebuildFunctionalSlots(slots, h) {
-    const children = [];
-    for (const slot in slots) {
-        if (slots.hasOwnProperty(slot)) {
-            children.push(h('template', { slot }, slots[slot]));
-        }
+export default function rebuildFunctionalSlots (slots, h) {
+  const children = []
+  for (const slot in slots) {
+    if (slots.hasOwnProperty(slot)) {
+      children.push(h('template', { slot }, slots[slot]))
     }
-    return children;
+  }
+  return children
 }
